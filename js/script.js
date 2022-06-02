5648b2d1b6de73f6e598f3cc9e9d32f76bfb792a
AlexTrinityBlock <q123717111@gmail.com>
2022-06-02 18:35:16 +0800

    t

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ new WOW().init();
 //載入圖標
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
+        // document.querySelector("body").style.visibility = "hidden";
         document.querySelector("#loader-c").style.visibility = "visible";
     } else {        
-        document.querySelector("body").style.visibility = "visible";
+        // document.querySelector("body").style.visibility = "visible";
         document.querySelector("#loader-c").style.display = "none";
     }
 };
